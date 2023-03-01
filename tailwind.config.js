452d7e201223c8eeb42689d2779b1ba3ba36736d
HadiUzzaman04 <hadi24x7@gmail.com>
2023-03-01 17:42:40 +0600

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ['./*.{html,js}'],
   theme: {
+    screens: {
+      xs: {'min': '320px', 'max': '480px'},
+      // => @media (min-width: 320px) { ... }
+
+      sm: {'min': '481px', 'max': '768px'},
+      // => @media (min-width: 640px) { ... }
+      md: {'min': '769px', 'max': '1024px'},  
+      // => @media (min-width: 768px) { ... }
+
+      lg: {'min': '1025px', 'max': '1440px'},
+      
+      // => @media (min-width: 1024px) { ... }
+      xl: {'min': '1441px','max': '2500px'},
+      // => @media (min-width: 1280px) { ... } 
+      xxl: {'min': '2500px','max': '2561px'},    
+    },
     extend: {},
   },
   plugins: [],
